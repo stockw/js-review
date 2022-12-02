e9d6713d6316ac8d4288d66996717d318157114c
stockw <courtney.wilkins05@gmail.com>
2022-12-02 11:25:02 -0500

    first commit

---
+++ review.js
@@ -0,0 +1,47 @@
+//Section 1 - A
+// 1) using (=) 
+// 2) Select the value and insert the new variable
+// 3) var myVar
+// 4) let myCar = "run"
+// 5) It is a text-based code that helps perform algorithms. It is used when we wanting to run a series of codes
+// 6) 90%
+
+//Section 1 - B
+// 1)
+let firstVariable = "Hello World";
+firstVariable = 10;
+
+let secondVariable = firstVariable;
+secondVariable = "Another number";
+
+let yourName = "Courtney";
+let greeting = "Hello, my name is " + yourName;
+
+// Section C
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a<b);
+console.log(c>d);
+console.log('Name'=='Name');
+console.log(true || false)
+console.log(false && false && false && false && false || true);
+console.log(false == false)
+console.log(e =='Kevin');
+console.log(a + b == c);
+console.log(a * a == d);
+console.log(48 == '48');
+
+// Section D
+let animal = "cow"
+
+if (animal == "cow"){
+    console.log("moooo");}
+    else{
+        console.log("Hey! You're not a cow");
+    }
+
+
